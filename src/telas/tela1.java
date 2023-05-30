d677eb5f6ce0108792e71e2fb1df5d83b4fb5dd6
RobertinCarlos <shakii876954@gmail.com>
2023-05-30 13:03:29 -0300

    commit

--- src/telas/tela1.java
+++ src/telas/tela1.java
@@ -1,16 +1,19 @@
 package telas;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 
 public class tela1 extends JPanel{
     Screen scrn;
-    Object[] buttons = {
-        "aaa", 
+    String[] buttons = {
+        "aaa", "baaa"
     };
+    GridBagLayout grid = new GridBagLayout();
 
     public void waaea(){
         System.out.println("aaa");
@@ -24,7 +27,12 @@ public class tela1 extends JPanel{
         this.setSize(this.scrn.getSize());
         this.setBounds(this.scrn.getBounds());
         this.setBackground(new Color(255,2,25));
-        this.setLayout(new GridLayout(3,2));
+        this.setLayout(grid);
+
+        for (String button : buttons) {
+            JButton b = new JButton(button);
+            this.add(b);
+        }
     }
     
     public void show(int index){
